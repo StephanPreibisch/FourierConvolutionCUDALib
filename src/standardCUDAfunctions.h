5ee1d6c282a5d97daed1490483285a34ef358138
Peter Steinbach <steinbach@scionics.de>
2015-01-09 11:48:33 +0100

    no need to touch code on windows anymore, waiting for validation

--- src/standardCUDAfunctions.h
+++ src/standardCUDAfunctions.h
@@ -8,12 +8,17 @@
 #ifndef STANDARDCUDAFUNCTIONS_H_
 #define STANDARDCUDAFUNCTIONS_H_
 
+#ifdef _WIN32
+#define FUNCTION_PREFIX extern "C" __declspec(dllexport)
+#else
+#define FUNCTION_PREFIX extern "C"
+#endif
 //----------------------------------functions to decide whhich GPU to use-------------------------------
 
-extern "C" int getCUDAcomputeCapabilityMinorVersion(int devCUDA);
-extern "C" int getCUDAcomputeCapabilityMajorVersion(int devCUDA);
-extern "C" int getNumDevicesCUDA();
-extern "C" void getNameDeviceCUDA(int devCUDA, char *name);
-extern "C" long long int getMemDeviceCUDA(int devCUDA);
+FUNCTION_PREFIX int getCUDAcomputeCapabilityMinorVersion(int devCUDA);
+FUNCTION_PREFIX int getCUDAcomputeCapabilityMajorVersion(int devCUDA);
+FUNCTION_PREFIX int getNumDevicesCUDA();
+FUNCTION_PREFIX void getNameDeviceCUDA(int devCUDA, char *name);
+FUNCTION_PREFIX long long int getMemDeviceCUDA(int devCUDA);
 
 #endif /* STANDARDCUDAFUNCTIONS_H_ */
