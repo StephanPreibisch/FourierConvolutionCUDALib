3443bea7169969abbd169ff7679c1a63d39e8476
Peter Steinbach <steinbac@mpi-cbg.de>
2015-04-23 10:46:56 +0200

    added new file that contains padding utils, completed testing suite for GPU based convolution

---
+++ tests/padd_utils.h
@@ -0,0 +1,169 @@
+#ifndef _PADD_UTILS_H_
+#define _PADD_UTILS_H_
+#include <vector>
+#include <algorithm>
+//#include <type_traits>
+#include <limits>
+#include "boost/multi_array.hpp"
+
+namespace multiviewnative {
+
+
+template <typename outT>
+struct add_minus_1 {
+
+  template <typename any_type>
+  outT operator()(const any_type& _first, const any_type& _second) {
+    return _first + _second - 1;
+  }
+};
+
+template <typename inT, typename outT>
+struct minus_1_div_2 {
+
+  outT operator()(const inT& _first) { return (_first - 1) / 2; }
+};
+
+template <typename ImageStackT>
+struct no_padd {
+
+  typedef typename ImageStackT::value_type value_type;
+  typedef typename ImageStackT::size_type size_type;
+  typedef typename ImageStackT::template array_view<
+      ImageStackT::dimensionality>::type image_stack_view;
+  typedef boost::multi_array_types::index_range range;
+
+  std::vector<size_type> extents_;
+  std::vector<size_type> offsets_;
+
+  no_padd()
+      : extents_(ImageStackT::dimensionality, 0),
+        offsets_(ImageStackT::dimensionality, 0) {}
+
+  template <typename T, typename U>
+  no_padd(T* _image_shape, U* _kernel_shape)
+      : extents_(_image_shape, _image_shape + ImageStackT::dimensionality),
+        offsets_(ImageStackT::dimensionality, 0) {
+    // static_assert(
+    //     std::numeric_limits<T>::is_integer,
+    //     "[no_padd] didn't receive integer type as image shape descriptor");
+    // static_assert(
+    //     std::numeric_limits<U>::is_integer,
+    //     "[no_padd] didn't receive integer type as kernel shape descriptor");
+  }
+
+  template <typename ImageStackRefT, typename OtherStackT>
+  void insert_at_offsets(const ImageStackRefT& _source, OtherStackT& _target) {
+    _target = _source;
+  }
+
+ 
+
+  const size_type* offsets() const { return &offsets_[0]; }
+
+  const size_type* extents() const { return &extents_[0]; }
+};
+
+template <typename ImageStackT>
+struct zero_padd {
+
+  typedef typename ImageStackT::value_type value_type;
+  typedef typename ImageStackT::size_type size_type;
+  typedef typename ImageStackT::template array_view<
+      ImageStackT::dimensionality>::type image_stack_view;
+  typedef boost::multi_array_types::index_range range;
+
+  std::vector<size_type> extents_;
+  std::vector<size_type> offsets_;
+
+  zero_padd()
+      : extents_(ImageStackT::dimensionality, 0),
+        offsets_(ImageStackT::dimensionality, 0) {}
+
+  zero_padd(const zero_padd& _other)
+      : extents_(_other.extents_), offsets_(_other.offsets_) {}
+
+  template <typename T, typename U>
+  zero_padd(T* _image, U* _kernel)
+      : extents_(ImageStackT::dimensionality, 0),
+        offsets_(ImageStackT::dimensionality, 0) {
+    // static_assert(
+    //     std::numeric_limits<T>::is_integer == true,
+    //     "[zero_padd] didn't receive integer type as image shape descriptor");
+    // static_assert(
+    //     std::numeric_limits<U>::is_integer,
+    //     "[zero_padd] didn't receive integer type as kernel shape descriptor");
+
+    std::transform(_image, _image + ImageStackT::dimensionality, _kernel,
+                   extents_.begin(), add_minus_1<T>());
+
+    std::transform(_kernel, _kernel + ImageStackT::dimensionality,
+                   offsets_.begin(), minus_1_div_2<U, size_type>());
+  }
+
+  zero_padd& operator=(const zero_padd& _other) {
+    if (this != &_other) {
+      extents_ = _other.extents_;
+      offsets_ = _other.offsets_;
+    }
+    return *this;
+  }
+
+  /**
+     \brief function that inserts _source in _target given limits by vector
+     offsets,
+     _target is expected to have dimensions
+     (source.shape()[0]+2*offsets_[0])x(source.shape()[1]+2*offsets_[1])x...
+     _target is hence expected to have dimensions extents_[0]xextents_[1]x...
+
+     as an example:
+     _source =
+     1 1
+     1 1
+     given a kernel of 3x3x3
+     would expecte a _target of
+     0 0 0 0
+     0 0 0 0
+     0 0 0 0
+     0 0 0 0
+
+     and the result would look like
+     0 0 0 0
+     0 1 1 0
+     0 1 1 0
+     0 0 0 0
+
+     \param[in] _source image stack to embed into target
+     \param[in] _target image stack of size extents_[0]xextents_[1]x...
+
+     \return
+     \retval
+
+  */
+  template <typename ImageStackRefT, typename OtherStackT>
+  void insert_at_offsets(const ImageStackRefT& _source, OtherStackT& _target) {
+
+    if (std::lexicographical_compare(
+            _target.shape(), _target.shape() + OtherStackT::dimensionality,
+            extents_.begin(), extents_.end()))
+      throw std::runtime_error(
+          "multiviewnative::zero_padd::insert_at_offsets]\ttarget image stack "
+          "is smaller or equal in size than source\n");
+
+    image_stack_view subview_padded_image = _target
+        [boost::indices[range(offsets_[0], offsets_[0] + _source.shape()[0])]
+                       [range(offsets_[1], offsets_[1] + _source.shape()[1])]
+                       [range(offsets_[2], offsets_[2] + _source.shape()[2])]];
+    subview_padded_image = _source;
+  }
+
+
+
+  const size_type* offsets() const { return &offsets_[0]; }
+
+  const size_type* extents() const { return &extents_[0]; }
+
+  virtual ~zero_padd() {};
+};
+}
+#endif /* _PADD_UTILS_H_ */
--- tests/test_gpu_convolve.cpp
+++ tests/test_gpu_convolve.cpp
@@ -2,6 +2,7 @@
 #define BOOST_TEST_MODULE GPU_CONVOLUTION
 #include "boost/test/unit_test.hpp"
 #include "test_fixtures.hpp"
+#include "padd_utils.h"
 #include <numeric>
 #include "convolution3Dfft.h"
 
@@ -11,15 +12,182 @@ BOOST_FIXTURE_TEST_SUITE(legacy_convolution,
 BOOST_AUTO_TEST_CASE(trivial_convolve) {
 
   float* image = image_.data();
-  float* kernel = new float[kernel_size_];
-  std::fill(kernel, kernel + kernel_size_, 0.f);
+  std::vector<float> kernel(kernel_size_,0);
 
-  convolution3DfftCUDAInPlace(image, &image_dims_[0], kernel, &kernel_dims_[0],
+  convolution3DfftCUDAInPlace(image, &image_dims_[0], 
+			      &kernel[0], &kernel_dims_[0],
                               selectDeviceWithHighestComputeCapability());
 
   float sum = std::accumulate(image, image + image_size_, 0.f);
   BOOST_CHECK_CLOSE(sum, 0.f, .00001);
 
-  delete[] kernel;
 }
+
+
+BOOST_AUTO_TEST_CASE(identity_convolve) {
+
+  using namespace multiviewnative;
+
+  float sum_expected = std::accumulate(
+      image_.data(), image_.data() + image_.num_elements(), 0.f);
+
+  zero_padd<image_stack> padder(image_.shape(), identity_kernel_.shape());
+  image_stack padded_image(padder.extents_, image_.storage_order());
+  padder.insert_at_offsets(image_, padded_image);
+
+  std::vector<int> extents_as_int(padder.extents_.size());
+  std::copy(padder.extents_.begin(), padder.extents_.end(),
+            extents_as_int.begin());
+
+  convolution3DfftCUDAInPlace(padded_image.data(), &extents_as_int[0],
+                              identity_kernel_.data(), &kernel_dims_[0],
+                              selectDeviceWithHighestComputeCapability());
+
+  float sum = std::accumulate(image_.data(),
+                              image_.data() + image_.num_elements(), 0.f);
+  BOOST_CHECK_CLOSE(sum, sum_expected, .00001);
+}
+
+BOOST_AUTO_TEST_CASE(horizontal_convolve) {
+  using namespace multiviewnative;
+
+  float sum_expected =
+      std::accumulate(image_folded_by_horizontal_.data(),
+                      image_folded_by_horizontal_.data() +
+                          image_folded_by_horizontal_.num_elements(),
+                      0.f);
+
+  zero_padd<image_stack> padder(image_.shape(), horizont_kernel_.shape());
+  image_stack padded_image(padder.extents_, image_.storage_order());
+
+  padder.insert_at_offsets(image_, padded_image);
+
+  std::vector<int> extents_as_int(padder.extents_.size());
+  std::copy(padder.extents_.begin(), padder.extents_.end(),
+            extents_as_int.begin());
+
+  convolution3DfftCUDAInPlace(padded_image.data(), &extents_as_int[0],
+                              horizont_kernel_.data(), &kernel_dims_[0],
+                              selectDeviceWithHighestComputeCapability());
+
+  image_ = padded_image
+      [boost::indices
+           [range(padder.offsets()[0], padder.offsets()[0] + image_dims_[0])]
+           [range(padder.offsets()[1], padder.offsets()[1] + image_dims_[1])]
+           [range(padder.offsets()[2], padder.offsets()[2] + image_dims_[2])]];
+
+  float sum = std::accumulate(image_.data(),
+                              image_.data() + image_.num_elements(), 0.f);
+
+  BOOST_REQUIRE_CLOSE(sum, sum_expected, .00001);
+}
+
+BOOST_AUTO_TEST_CASE(vertical_convolve) {
+
+  multiviewnative::zero_padd<multiviewnative::image_stack> padder(
+      image_.shape(), vertical_kernel_.shape());
+  multiviewnative::image_stack padded_image(padder.extents_,
+                                            image_.storage_order());
+
+  padder.insert_at_offsets(image_, padded_image);
+
+  std::vector<int> extents_as_int(padder.extents_.size());
+  std::copy(padder.extents_.begin(), padder.extents_.end(),
+            extents_as_int.begin());
+
+  convolution3DfftCUDAInPlace(padded_image.data(), &extents_as_int[0],
+                              vertical_kernel_.data(), &kernel_dims_[0],
+                              selectDeviceWithHighestComputeCapability());
+
+  float sum_expected =
+      std::accumulate(image_folded_by_vertical_.data(),
+                      image_folded_by_vertical_.data() +
+                          image_folded_by_vertical_.num_elements(),
+                      0.f);
+
+  image_ = padded_image
+      [boost::indices
+           [multiviewnative::range(padder.offsets()[0],
+                                   padder.offsets()[0] + image_dims_[0])]
+           [multiviewnative::range(padder.offsets()[1],
+                                   padder.offsets()[1] + image_dims_[1])]
+           [multiviewnative::range(padder.offsets()[2],
+                                   padder.offsets()[2] + image_dims_[2])]];
+
+  float sum = std::accumulate(image_.data(),
+                              image_.data() + image_.num_elements(), 0.f);
+  BOOST_CHECK_CLOSE(sum, sum_expected, .00001);
+}
+
+BOOST_AUTO_TEST_CASE(depth_convolve) {
+
+  multiviewnative::zero_padd<multiviewnative::image_stack> padder(
+      image_.shape(), depth_kernel_.shape());
+  multiviewnative::image_stack padded_image(padder.extents_,
+                                            image_.storage_order());
+
+  padder.insert_at_offsets(image_, padded_image);
+
+  std::vector<int> extents_as_int(padder.extents_.size());
+  std::copy(padder.extents_.begin(), padder.extents_.end(),
+            extents_as_int.begin());
+
+  convolution3DfftCUDAInPlace(padded_image.data(), &extents_as_int[0],
+                              depth_kernel_.data(), &kernel_dims_[0],
+                              selectDeviceWithHighestComputeCapability());
+
+  float sum_expected = std::accumulate(
+      image_folded_by_depth_.data(),
+      image_folded_by_depth_.data() + image_folded_by_depth_.num_elements(),
+      0.f);
+
+  image_ = padded_image
+      [boost::indices
+           [multiviewnative::range(padder.offsets()[0],
+                                   padder.offsets()[0] + image_dims_[0])]
+           [multiviewnative::range(padder.offsets()[1],
+                                   padder.offsets()[1] + image_dims_[1])]
+           [multiviewnative::range(padder.offsets()[2],
+                                   padder.offsets()[2] + image_dims_[2])]];
+
+  float sum = std::accumulate(image_.data(),
+                              image_.data() + image_.num_elements(), 0.f);
+  BOOST_CHECK_CLOSE(sum, sum_expected, .00001);
+}
+
+BOOST_AUTO_TEST_CASE(all1_convolve) {
+
+  multiviewnative::zero_padd<multiviewnative::image_stack> padder(
+      image_.shape(), all1_kernel_.shape());
+  multiviewnative::image_stack padded_image(padder.extents_,
+                                            image_.storage_order());
+
+  padder.insert_at_offsets(image_, padded_image);
+
+  std::vector<int> extents_as_int(padder.extents_.size());
+  std::copy(padder.extents_.begin(), padder.extents_.end(),
+            extents_as_int.begin());
+
+  convolution3DfftCUDAInPlace(padded_image.data(), &extents_as_int[0],
+                              all1_kernel_.data(), &kernel_dims_[0],
+                              selectDeviceWithHighestComputeCapability());
+
+  float sum_expected = std::accumulate(
+      image_folded_by_all1_.data(),
+      image_folded_by_all1_.data() + image_folded_by_all1_.num_elements(), 0.f);
+
+  image_ = padded_image
+      [boost::indices
+           [multiviewnative::range(padder.offsets()[0],
+                                   padder.offsets()[0] + image_dims_[0])]
+           [multiviewnative::range(padder.offsets()[1],
+                                   padder.offsets()[1] + image_dims_[1])]
+           [multiviewnative::range(padder.offsets()[2],
+                                   padder.offsets()[2] + image_dims_[2])]];
+
+  float sum = std::accumulate(image_.data(),
+                              image_.data() + image_.num_elements(), 0.f);
+  BOOST_CHECK_CLOSE(sum, sum_expected, .00001);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
