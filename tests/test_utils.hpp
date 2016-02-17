943a6fb600f71e0b17f848a68b1bf0813373fed6
Peter Steinbach <steinbach@scionics.de>
2016-02-17 09:18:22 +0100

    moved test fixtures and l2norm functor etc into their header

--- tests/test_utils.hpp
+++ tests/test_utils.hpp
@@ -1,67 +1,69 @@
 #ifndef _TEST_UTILS_H_
 #define _TEST_UTILS_H_
 
+namespace fourierconvolution {
 
-template <int value = 42>
-struct set_to_float {
+  template <int value = 42>
+  struct set_to_float {
 
-  float operator()(){
+    float operator()(){
 
-    return float(value);
+      return float(value);
     
-  }
+    }
   
-};
+  };
 
-template<typename in_type, typename out_type = in_type>
-struct diff_squared {
+  template<typename in_type, typename out_type = in_type>
+  struct diff_squared {
 
-  out_type operator()(const in_type& _first, const in_type& _second){
+    out_type operator()(const in_type& _first, const in_type& _second){
 
-    out_type value = _first - _second;
-    return (value*value);
+      out_type value = _first - _second;
+      return (value*value);
     
-  }
+    }
   
-};
+  };
 
 
 
-struct ramp
-{
-  size_t value;
+  struct ramp
+  {
+    size_t value;
 
-  ramp():
-    value(0){};
+    ramp():
+      value(0){};
   
-  float operator()(){
+    float operator()(){
 
-    return value++;
+      return value++;
     
-  }
+    }
   
-};
+  };
   
 
-template <typename value_policy = ramp>
-struct stack_fixture {
+  template <typename stack_type, typename value_policy = ramp>
+  struct stack_fixture {
 
-  fc::image_stack stack;
-  fc::image_stack kernel;
+  
+    stack_type stack;
+    stack_type kernel;
 
-  template <typename T>
-  stack_fixture(const std::vector<T>& _stack_shape,
-	       const std::vector<T>& _kernel_shape):
-    stack(_stack_shape),
-    kernel(_kernel_shape){
+    template <typename T>
+    stack_fixture(const std::vector<T>& _stack_shape,
+		  const std::vector<T>& _kernel_shape):
+      stack(_stack_shape),
+      kernel(_kernel_shape){
 
-    value_policy operation;
-    std::fill(kernel.data(),kernel.data()+kernel.num_elements(),0);
-    std::generate(stack.data(),stack.data()+stack.num_elements(),operation);
+      value_policy operation;
+      std::fill(kernel.data(),kernel.data()+kernel.num_elements(),0);
+      std::generate(stack.data(),stack.data()+stack.num_elements(),operation);
     
-  }
+    }
   
+  };
+
 };
-
-
 #endif /* _TEST_UTILS_H_ */
