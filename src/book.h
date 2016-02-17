7c2acb4ffd60d7313d22ff62e6dcafd50df62e78
Peter Steinbach <steinbach@scionics.de>
2016-02-17 16:32:01 +0100

    added new CUFFT only safe guards (in a very unclean)

--- src/book.h
+++ src/book.h
@@ -17,6 +17,7 @@
 #ifndef __BOOK_H__
 #define __BOOK_H__
 #include <stdio.h>
+#include <cufft.h>
 
 static void HandleError( cudaError_t err,
                          const char *file,
@@ -28,14 +29,97 @@ static void HandleError( cudaError_t err,
     }
 }
 
+static const char *_cudaGetErrorEnum(cufftResult error)
+{
+    switch (error)
+    {
+        case CUFFT_SUCCESS:
+            return "CUFFT_SUCCESS";
+
+        case CUFFT_INVALID_PLAN:
+            return "CUFFT_INVALID_PLAN";
+
+        case CUFFT_ALLOC_FAILED:
+            return "CUFFT_ALLOC_FAILED";
+
+        case CUFFT_INVALID_TYPE:
+            return "CUFFT_INVALID_TYPE";
+
+        case CUFFT_INVALID_VALUE:
+            return "CUFFT_INVALID_VALUE";
+
+        case CUFFT_INTERNAL_ERROR:
+            return "CUFFT_INTERNAL_ERROR";
+
+        case CUFFT_EXEC_FAILED:
+            return "CUFFT_EXEC_FAILED";
+
+        case CUFFT_SETUP_FAILED:
+            return "CUFFT_SETUP_FAILED";
+
+        case CUFFT_INVALID_SIZE:
+            return "CUFFT_INVALID_SIZE";
+
+        case CUFFT_UNALIGNED_DATA:
+            return "CUFFT_UNALIGNED_DATA";
+
+        case CUFFT_INCOMPLETE_PARAMETER_LIST:
+            return "CUFFT_INCOMPLETE_PARAMETER_LIST";
+
+        case CUFFT_INVALID_DEVICE:
+            return "CUFFT_INVALID_DEVICE";
+
+        case CUFFT_PARSE_ERROR:
+            return "CUFFT_PARSE_ERROR";
+
+        case CUFFT_NO_WORKSPACE:
+            return "CUFFT_NO_WORKSPACE";
+
+        case CUFFT_NOT_IMPLEMENTED:
+            return "CUFFT_NOT_IMPLEMENTED";
+
+        case CUFFT_LICENSE_ERROR:
+            return "CUFFT_LICENSE_ERROR";
+    }
+
+    return "<unknown>";
+}
+
+static void HandleCufftError( cufftResult err,
+                         const char *file,
+                         int line ) {
+    if (err != CUFFT_SUCCESS) {
+        printf( "%s in %s at line %d\n", _cudaGetErrorEnum( err ),
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
+
+
 #define HANDLE_ERROR_KERNEL HandleError(cudaPeekAtLastError(),__FILE__, __LINE__ )
 
 #define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+#define CUFFT_ERROR( err ) (HandleCufftError( err, __FILE__, __LINE__ ))
 
 #define HANDLE_NULL( a ) {if (a == NULL) { \
                             printf( "Host memory failed in %s at line %d\n", \
                                     __FILE__, __LINE__ ); \
                             exit( EXIT_FAILURE );}}
 
+#include <stdexcept>
+#include <sstream>
+
+static void throw_on_cufft_error( cufftResult err,
+				  const char *file,
+				  int line ) {
+  if (err != CUFFT_SUCCESS) {
+    std::ostringstream msg;
+    msg << _cudaGetErrorEnum( err ) << " in " << file << " at line " << line << "\n";
+    
+    throw std::runtime_error(msg.str());
+  }
+}
+
+#define THROW_CUFFT_ERROR( err ) (throw_on_cufft_error( err, __FILE__, __LINE__ ))
 
 #endif  // __BOOK_H__
